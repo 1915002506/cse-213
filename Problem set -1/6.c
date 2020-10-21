83c9da81e4fd85f97e28a1e959da8d968cf64e60
1915002506 <72783875+1915002506@users.noreply.github.com>
2020-10-21 17:03:39 +0600

    Add files via upload

---
+++ Problem set -1/6.c
@@ -0,0 +1,38 @@
+/*
+Problem Name:List all character and integer Data Types Name, Format Specifier and Memory Size in Byte including Modifiers.
+Name:Supriya Debnath
+ID:1915002506
+*/
+#include<stdio.h>
+int main(){
+ /*
+     char a;
+     int b;
+     float c;
+     double d;
+     char f;
+
+     printf("\nSize of a:%d", sizeof(a));
+     printf("\nSize of b:%d", sizeof(b));
+     printf("\nSize of c:%d", sizeof(c));
+     printf("\nSize of d:%d", sizeof(d));
+     printf("\nSize of str:%d", sizeof(f));
+
+   return 0;
+} */
+
+ int a;
+ char c;
+ float f;
+ double d;
+ char b;
+
+     printf("\nSize of a:%d", sizeof(a));
+     printf("\nSize of c:%d", sizeof(c));
+     printf("\nSize of f:%d", sizeof(f));
+     printf("\nSize of d:%d", sizeof(d));
+     printf("\nSize of str:%d", sizeof(b));
+
+     return 0;
+     }
+
